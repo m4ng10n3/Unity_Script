b0f9ed11af8b204982868b00a8d0e4904bc99ec3
m4ng10n3 <89881295+m4ng10n3@users.noreply.github.com>
2025-10-01 09:13:04 -0700

    feat(anim): aggiunti trigger attacchi multipli e eventi per finestra danno

--- Enemy_AC.cs
+++ Enemy_AC.cs
@@ -6,11 +6,13 @@ public class EnemyAnimatorLink : MonoBehaviour
 {
     private Animator anim;
     private Rigidbody2D rb;
+    private EnemyController controller;
 
     private void Awake()
     {
         anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
+        controller = GetComponent<EnemyController>();
     }
 
     private void Update()
@@ -20,5 +22,26 @@ public class EnemyAnimatorLink : MonoBehaviour
 
     public void PlayAttack() => anim.SetTrigger("attack");
     public void PlayDie() => anim.SetTrigger("die");
+
+    // New attack triggers for multiple attack types
+    public void PlayAttackClose() => anim.SetTrigger("attackClose");
+    public void PlayAttackMid() => anim.SetTrigger("attackMid");
+    public void PlayAttackJump() => anim.SetTrigger("attackJump");
+
+    // Animation events to control damage window
+    public void AE_OpenDamageWindow()
+    {
+        if (controller != null)
+        {
+            controller.OpenDamageWindow();
+        }
+    }
+
+    public void AE_CloseDamageWindow()
+    {
+        if (controller != null)
+        {
+            controller.CloseDamageWindow();
+        }
+    }
 }
-//pippo
